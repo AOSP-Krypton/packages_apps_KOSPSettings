b145f74475de5d1b98061b01dbd6855d92be017a
jhonboy121 <alfredmathew05@gmail.com>
2021-09-16 17:11:40 +0530

    KryptonSettings: pull theme settings from Settings
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/krypton/settings/fragment/ThemeSettingsFragment.java
+++ src/com/krypton/settings/fragment/ThemeSettingsFragment.java
@@ -15,22 +15,24 @@
  */
 package com.krypton.settings.fragment;
 
-import android.os.Bundle;
+import android.content.Context;
 
 import androidx.preference.Preference;
 
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.development.AccentOverlayCategoryPreferenceController;
+import com.android.settings.development.OverlayCategoryPreferenceController;
 import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
 import com.krypton.settings.fragment.AccentPickerFragment;
 import com.krypton.settings.preference.AccentPickerPreference;
 
-public class ThemeSettingsFragment extends BaseFragment {
-    private static final String TAG = "ThemeSettingsFragment";
+import java.util.ArrayList;
+import java.util.List;
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.theme_settings);
-    }
+public class ThemeSettingsFragment extends DashboardFragment {
+    private static final String TAG = "ThemeSettingsFragment";
 
     @Override
     public void onDisplayPreferenceDialog(Preference preference) {
@@ -41,5 +43,32 @@ public class ThemeSettingsFragment extends BaseFragment {
         } else {
             super.onDisplayPreferenceDialog(preference);
         }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.KRYPTON;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.theme_settings;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>(3);
+        controllers.add(new AccentOverlayCategoryPreferenceController(context,
+                "android.theme.customization.accent_color"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.adaptive_icon_shape"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.icon_pack"));
+        return controllers;
     }
 }
