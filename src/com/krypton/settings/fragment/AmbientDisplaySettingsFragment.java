ba830915c644161272b8f14eca4d5881f081f484
jhonboy121 <alfredmathew05@gmail.com>
2021-09-21 00:26:55 +0530

    KryptonSettings: relocated aod prefs to Settings
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/krypton/settings/fragment/AmbientDisplaySettingsFragment.java
+++ src/com/krypton/settings/fragment/AmbientDisplaySettingsFragment.java
@@ -16,8 +16,6 @@
 package com.krypton.settings.fragment;
 
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.provider.Settings;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.preference.Preference;
@@ -29,22 +27,11 @@ import com.krypton.settings.preference.SettingColorPickerPreference;
 public class AmbientDisplaySettingsFragment extends BaseFragment {
 
     private static final String TAG = "AmbientDisplaySettingsFragment";
-    private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
-
-    static final int MODE_DISABLED = 0;
-    static final int MODE_NIGHT = 1;
-    static final int MODE_TIME = 2;
-    static final int MODE_MIXED_SUNSET = 3;
-    static final int MODE_MIXED_SUNRISE = 4;
-
-    private Preference mAODPref;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.ambient_display_settings);
-        mAODPref = findPreference(AOD_SCHEDULE_KEY);
-        updateAlwaysOnSummary();
     }
 
     @Override
@@ -56,36 +43,6 @@ public class AmbientDisplaySettingsFragment extends BaseFragment {
             fragment.show(getParentFragmentManager(), TAG);
         } else {
             super.onDisplayPreferenceDialog(preference);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        updateAlwaysOnSummary();
-    }
-
-    private void updateAlwaysOnSummary() {
-        if (mAODPref == null) return;
-        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
-                Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
-        switch (mode) {
-            default:
-            case MODE_DISABLED:
-                mAODPref.setSummary(R.string.disabled);
-                break;
-            case MODE_NIGHT:
-                mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
-                break;
-            case MODE_TIME:
-                mAODPref.setSummary(R.string.night_display_auto_mode_custom);
-                break;
-            case MODE_MIXED_SUNSET:
-                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunset);
-                break;
-            case MODE_MIXED_SUNRISE:
-                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunrise);
-                break;
         }
     }
 }
