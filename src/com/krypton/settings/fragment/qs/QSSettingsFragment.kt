0393a563c716e8362f4654d391c4ec292c922a16
jhonboy121 <alfredmathew05@gmail.com>
2021-12-14 15:57:57 +0530

    KryptonSettings: separate out statusbar and qs customisations
    * icons from https://github.com/yaap/packages_apps_YASP/blob/twelve/res/drawable
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ src/com/krypton/settings/fragment/qs/QSSettingsFragment.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.krypton.settings.fragment.qs
+
+import com.android.settings.R
+import com.krypton.settings.fragment.KryptonDashboardFragment
+
+class QSSettingsFragment: KryptonDashboardFragment() {
+    override protected fun getPreferenceScreenResId() = R.xml.qs_settings
+
+    override protected fun getLogTag() = TAG
+
+    companion object {
+        private const val TAG = "QSSettingsFragment"
+    }
+}
