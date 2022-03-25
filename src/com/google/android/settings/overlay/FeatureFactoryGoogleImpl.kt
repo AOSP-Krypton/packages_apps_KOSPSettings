c3d713f80ea04b72e76a7d3ddc238139581e7ef4
Anay Wadhera <anay1018@gmail.com>
2022-03-25 19:39:56 +0530

    FlamingoSettings: use PowerUsageFeatureGoogle
    [jhonboy121]: rewrite in kt
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/google/android/settings/overlay/FeatureFactoryGoogleImpl.kt
+++ src/com/google/android/settings/overlay/FeatureFactoryGoogleImpl.kt
@@ -1,5 +1,19 @@
 package com.google.android.settings.overlay
 
-import com.android.settings.overlay.FeatureFactoryImpl
+import android.content.Context
 
-class FeatureFactoryGoogleImpl : FeatureFactoryImpl()
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider
+import com.android.settings.overlay.FeatureFactoryImpl
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl
+
+class FeatureFactoryGoogleImpl : FeatureFactoryImpl() {
+
+    private var powerUsageFeatureProviderGoogleImpl: PowerUsageFeatureProvider? = null
+
+    override fun getPowerUsageFeatureProvider(context: Context): PowerUsageFeatureProvider {
+        if (powerUsageFeatureProviderGoogleImpl == null) {
+            powerUsageFeatureProviderGoogleImpl = PowerUsageFeatureProviderGoogleImpl(context.applicationContext)
+        }
+        return powerUsageFeatureProviderGoogleImpl!!
+    }
+}
