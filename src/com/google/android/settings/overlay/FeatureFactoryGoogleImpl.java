4a1f3f7abd4f4ebefb975a81c9feb76689e49309
Anay Wadhera <anay1018@gmail.com>
2022-03-25 19:39:56 +0530

    KryptonSettings: use PowerUsageFeatureGoogle
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/google/android/settings/overlay/FeatureFactoryGoogleImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryGoogleImpl.java
@@ -1,6 +1,21 @@
 package com.google.android.settings.overlay;
 
+import android.content.Context;
+
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
 import com.android.settings.overlay.FeatureFactoryImpl;
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryGoogleImpl extends FeatureFactoryImpl {
+
+    private PowerUsageFeatureProviderGoogleImpl mPowerUsageFeatureProviderGoogleImpl;
+
+    @Override
+    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
+        if (mPowerUsageFeatureProviderGoogleImpl == null) {
+            mPowerUsageFeatureProviderGoogleImpl = new PowerUsageFeatureProviderGoogleImpl(
+                context.getApplicationContext());
+        }
+        return mPowerUsageFeatureProviderGoogleImpl;
+    }
 }
