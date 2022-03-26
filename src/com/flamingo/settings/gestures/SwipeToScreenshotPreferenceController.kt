f4677c5a4a2e6355f82a5c4831171d418fcdfbab
ghbhaha <ghbhaha@gmail.com>
2022-03-26 21:54:40 +0530

    FlamingoSettings: Add three-fingers-swipe to screenshot [2/2]
    * Thanks Sahul Krishan for the beautiful animation
    * @jhonboy121: adapt to A12L, cleanup and rewrite in kt
    
    Signed-off-by: ghbhaha <ghbhaha@gmail.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ src/com/flamingo/settings/gestures/SwipeToScreenshotPreferenceController.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.flamingo.settings.gestures
+
+import android.content.Context
+import android.provider.Settings
+import android.provider.Settings.System.THREE_FINGER_GESTURE
+
+import com.android.settings.R
+import com.android.settings.gestures.GesturePreferenceController
+
+class SwipeToScreenshotPreferenceController(
+    context: Context, preferenceKey: String
+) : GesturePreferenceController(context, preferenceKey) {
+
+    override fun getAvailabilityStatus(): Int = AVAILABLE
+
+    override fun isSliceable() = true
+
+    override protected fun getVideoPrefKey() = PREF_KEY_VIDEO
+
+    override fun setChecked(isChecked: Boolean): Boolean =
+        Settings.System.putInt(mContext.contentResolver,
+            THREE_FINGER_GESTURE, if (isChecked) 1 else 0)
+
+    override fun isChecked() =
+        Settings.System.getInt(mContext.contentResolver,
+            THREE_FINGER_GESTURE, 0) == 1
+
+    override fun getSliceHighlightMenuRes() = R.string.menu_key_system
+
+    companion object {
+        private const val PREF_KEY_VIDEO = "swipe_to_screenshot_video"
+    }
+}
---
+++ src/com/flamingo/settings/gestures/SwipeToScreenshotSettings.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.flamingo.settings.gestures
+
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.search.SearchIndexable
+import com.flamingo.settings.FlamingoDashboardFragment
+
+@SearchIndexable
+class SwipeToScreenshotSettings : FlamingoDashboardFragment() {
+
+    override protected fun getPreferenceScreenResId() = R.xml.swipe_to_screenshot_settings
+
+    override protected fun getLogTag() = TAG
+
+    companion object {
+        private const val TAG = "SwipeToScreenshotSettings"
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.swipe_to_screenshot_settings)
+    }
+}
