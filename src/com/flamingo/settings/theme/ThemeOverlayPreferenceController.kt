66777a1079ee1fc43291ceb1b194e949d0569050
jhonboy121 <alfredmathew05@gmail.com>
2022-06-01 21:33:30 +0530

    FlamingoSettings: add icon pack preference
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ src/com/flamingo/settings/theme/ThemeOverlayPreferenceController.kt
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.flamingo.settings.theme
+
+import android.content.Context
+import android.content.om.IOverlayManager
+import android.content.om.OverlayInfo
+import android.content.pm.PackageManager
+import android.os.RemoteException
+import android.os.ServiceManager
+import android.os.UserHandle
+import android.provider.Settings
+import android.util.Log
+
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+
+import com.android.settings.R
+import com.android.settings.core.BasePreferenceController
+
+import org.json.JSONException
+import org.json.JSONObject
+
+/**
+ * [Preference] controller for theme customisation overlays.
+ *
+ * @param context the [Context] for this controller.
+ * @param key the preference key.
+ * @param categoryPackageMap a [Map] of an overlays category to it's target package.
+ */
+class ThemeOverlayPreferenceController(
+    context: Context,
+    key: String,
+    private val categoryPackageMap: Map<String, String>
+) : BasePreferenceController(context, key),
+    Preference.OnPreferenceChangeListener {
+
+    private val overlayManager = IOverlayManager.Stub.asInterface(
+        ServiceManager.getService(Context.OVERLAY_SERVICE))
+    private val packageManager = context.packageManager
+    private val defaultLabel = context.getString(R.string.overlay_option_device_default)
+
+    /**
+     * [MutableMap] of overlay label to a list of [OverlayInfo]s with same label.
+     */
+    private var overlayInfos: MutableMap<String, MutableList<OverlayInfo>> = getOverlayInfos()
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun updateState(preference: Preference) {
+        val listPreference = preference as ListPreference
+        var labels = mutableListOf<String>()
+
+        var selectedLabel = defaultLabel
+
+        overlayInfos = getOverlayInfos()
+        overlayInfos.forEach { label, list ->
+            labels.add(label)
+            if (list.any { it.isEnabled() }) {
+                selectedLabel = label
+            }
+        }
+        // Sort the labels lexicographically.
+        labels.sort()
+        // Keep default one at top always
+        labels.add(0, defaultLabel)
+
+        listPreference.entries = Array(labels.size) { labels[it] }
+        listPreference.entryValues = listPreference.entries
+        listPreference.value = selectedLabel
+        listPreference.summary = selectedLabel
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        // Map of overlay category to overlay package stored as a [JSONObject] flattened to string
+        val overlayPackageJson: String? = Settings.Secure.getStringForUser(
+                mContext.contentResolver,
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                UserHandle.USER_CURRENT)
+        try {
+            val jsonObject = overlayPackageJson?.let { JSONObject(it) } ?: JSONObject()
+            newValue?.let { value ->
+                if (value == defaultLabel) {
+                    // User selected default, delete all elements for
+                    // the categories of this preference.
+                    categoryPackageMap.keys.forEach { category ->
+                        jsonObject.remove(category)
+                    }
+                    return@let
+                }
+                overlayInfos[value as String]?.let {
+                    categoryPackageMap.forEach { category, target ->
+                        val overlayInfo = it.find { oi -> oi.targetPackageName == target }
+                        if (overlayInfo == null) {
+                            // There is no overlay for this category, delete entry
+                            // if present so that system default will be used for it.
+                            jsonObject.remove(category)
+                        } else {
+                            jsonObject.put(category, overlayInfo.packageName)
+                        }
+                    }
+                }
+            }
+            Settings.Secure.putStringForUser(mContext.contentResolver,
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                jsonObject.toString(), UserHandle.USER_CURRENT)
+        } catch (e: JSONException) {
+            Log.e(TAG, "JSONException while processing json, ${e.message}")
+        }
+        return true
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun getOverlayInfos(): MutableMap<String, MutableList<OverlayInfo>> {
+        // A map of overlay name to all the OverlayInfo's with the same name
+        val filteredInfos = mutableMapOf<String, MutableList<OverlayInfo>>()
+        try {
+            categoryPackageMap.forEach { category, target ->
+                val overlays = overlayManager.getOverlayInfosForTarget(
+                        target, UserHandle.USER_SYSTEM) as List<OverlayInfo>
+                overlays.filter {
+                    // Filter based on category.
+                    it.category == category
+                }.forEach {
+                    try {
+                        val label = packageManager.getApplicationInfo(it.packageName,
+                            0).loadLabel(packageManager).toString()
+                        if (!filteredInfos.containsKey(label)) {
+                            filteredInfos.put(label, mutableListOf(it))
+                        } else {
+                            filteredInfos[label]!!.add(it)
+                        }
+                    } catch (_: PackageManager.NameNotFoundException) {}
+                }
+            }
+        } catch (e: RemoteException) {
+            Log.e(TAG, "RemoteException while getting overlay infos")
+        }
+        return filteredInfos
+    }
+
+    companion object {
+        private const val TAG = "ThemeOverlayPreferenceController"
+    }
+}
--- src/com/flamingo/settings/theme/ThemeSettings.kt
+++ src/com/flamingo/settings/theme/ThemeSettings.kt
@@ -16,8 +16,13 @@
 
 package com.flamingo.settings.theme
 
+import android.content.Context
+
+import com.android.internal.util.flamingo.FlamingoUtils
 import com.android.settings.R
 import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.core.lifecycle.Lifecycle
 import com.android.settingslib.search.SearchIndexable
 import com.flamingo.settings.FlamingoDashboardFragment
 
@@ -28,10 +33,76 @@ class ThemeSettings : FlamingoDashboardFragment() {
 
     override protected fun getLogTag() = TAG
 
+    override protected fun createPreferenceControllers(
+        context: Context
+    ): List<AbstractPreferenceController> = buildPreferenceControllers(
+        context,
+        this /* host */,
+        settingsLifecycle,
+    )
+
     companion object {
         private const val TAG = "ThemeSettings"
 
+        private const val OVERLAY_CATEGORY_ICON_ANDROID = "android.theme.customization.icon_pack.android"
+        private const val OVERLAY_CATEGORY_ICON_SYSUI = "android.theme.customization.icon_pack.systemui"
+        private const val OVERLAY_CATEGORY_ICON_SETTINGS = "android.theme.customization.icon_pack.settings"
+        private const val OVERLAY_CATEGORY_ICON_LAUNCHER = "android.theme.customization.icon_pack.launcher"
+        private const val OVERLAY_CATEGORY_ICON_THEME_PICKER = "android.theme.customization.icon_pack.themepicker"
+
+        private const val TARGET_ANDROID = "android"
+        private const val TARGET_SYSUI = "com.android.systemui"
+        private const val TARGET_SETTINGS = "com.android.settings"
+        private const val TARGET_LAUNCHER = "com.android.launcher3"
+        private const val TARGET_THEME_PICKER = "com.android.wallpaper"
+
+        private const val ICON_PACK_PREFERENCE_KEY = "icon_pack_preference"
+
+        private fun buildPreferenceControllers(
+            context: Context,
+            host: ThemeSettings?,
+            lifecycle: Lifecycle?,
+        ): List<AbstractPreferenceController> {
+            val isAospLauncherInstalled = FlamingoUtils.isPackageInstalled(
+                context,
+                TARGET_LAUNCHER,
+                false /** ignoreState */,
+            )
+            val isAospThemePickerInstalled = FlamingoUtils.isPackageInstalled(
+                context,
+                TARGET_THEME_PICKER,
+                false /** ignoreState */,
+            )
+            return listOf(
+                ThemeOverlayPreferenceController(
+                    context,
+                    ICON_PACK_PREFERENCE_KEY,
+                    mutableMapOf(
+                        OVERLAY_CATEGORY_ICON_ANDROID to TARGET_ANDROID,
+                        OVERLAY_CATEGORY_ICON_SYSUI to TARGET_SYSUI,
+                        OVERLAY_CATEGORY_ICON_SETTINGS to TARGET_SETTINGS,
+                    ).apply {
+                        // Conditionally add launcher and themepicker
+                        if (isAospLauncherInstalled) {
+                            put(OVERLAY_CATEGORY_ICON_LAUNCHER, TARGET_LAUNCHER)
+                        }
+                        if (isAospThemePickerInstalled) {
+                            put(OVERLAY_CATEGORY_ICON_THEME_PICKER, TARGET_THEME_PICKER)
+                        }
+                    }.toMap(),
+                )
+            )
+        }
+
         @JvmField
-        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.theme_settings)
+        val SEARCH_INDEX_DATA_PROVIDER = object : BaseSearchIndexProvider(R.xml.theme_settings) {
+            override fun createPreferenceControllers(
+                context: Context
+            ): List<AbstractPreferenceController> = buildPreferenceControllers(
+                context,
+                null /* host */,
+                null /* lifecycle */,
+            )
+        }
     }
 }
